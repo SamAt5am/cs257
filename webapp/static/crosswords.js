80fbed6cffdd50957c96d4be5b75eb88c030a42c
James Brink <jamesbrink@Big-Nintendo-DS.local>
2022-11-14 16:51:32 -0600

    updated js/html

--- webapp/static/crosswords.js
+++ webapp/static/crosswords.js
@@ -5,11 +5,13 @@
  */
 
 window.onload = initialize;
-console.log("starting..");
 
 function initialize() {
-    let element = document.getElementById('clues_button');
-    element.onclick = onCluesButtonCicked;
+
+    var button = document.getElementById('search-button');
+    button.onclick = onSearchButtonClick;
+    //let element = document.getElementById('clues_button');
+    //element.onclick = onCluesButtonCicked;
 }
 
 // Returns the base URL of the API, onto which endpoint
@@ -21,6 +23,144 @@ function getAPIBaseURL() {
                     + '/api';
     return baseURL;
 }
+
+function onSearchButtonClick() {
+    console.log('Search Button clicked!')
+    var searchElement = document.getElementById('search-box');
+    var searchSelect = document.getElementById('search-select');
+    console.log(searchElement.value)
+    console.log(searchSelect.value)
+
+    if (searchSelect.value == "answer") {
+        console.log("checking answers...")
+        onAnswersSearch(searchElement.value)
+    }
+    else if (searchSelect.value == "clue") {
+        console.log("checking clues...")
+        onCluesSearch(searchElement.value)
+    }
+    
+}
+function onAnswersSearch(searchTest) {
+
+    console.log("checking API for: " + searchTest);
+    var url = getAPIBaseURL() + '/answers/' + searchTest;
+
+    // Send the request to the Crosswords API /authors/ endpoint
+    fetch(url, {method: 'get'})
+
+    // When the results come back, transform them from a JSON string into
+    // a Javascript object (in this case, a list of author dictionaries).
+    .then((response) => response.json())
+
+    // Once you have your list of author dictionaries, use it to build
+    // an HTML table displaying the author names and lifespan.
+    .then(function(answerList) {
+        // Build the table body.
+        var tableBody = '';
+        for (var k = 0; k < answerList.length; k++) {
+
+            tableBody += '<tr>';
+            tableBody += '<td>' +  answerList[k]['answer'] + '</td>';
+            tableBody += '<td>' +  answerList[k]['clue'] + '</td>';
+            tableBody += '</tr>';
+        }
+
+        // Put the table body we just built inside the table that's already on the page.
+        var resultsTableElement = document.getElementById('answer_table');
+        if (resultsTableElement) {
+            resultsTableElement.innerHTML = tableBody;
+        }
+    })
+
+    // Log the error if anything went wrong during the fetch.
+    .catch(function(error) {
+        console.log(error);
+    });
+
+}
+
+function onCluesSearch(searchTest) {
+
+    console.log("checking API for: " + searchTest);
+    var url = getAPIBaseURL() + '/clues/' + searchTest;
+
+    // Send the request to the Crosswords API /authors/ endpoint
+    fetch(url, {method: 'get'})
+
+    // When the results come back, transform them from a JSON string into
+    // a Javascript object (in this case, a list of author dictionaries).
+    .then((response) => response.json())
+
+    // Once you have your list of author dictionaries, use it to build
+    // an HTML table displaying the author names and lifespan.
+    .then(function(clueList) {
+        // Build the table body.
+        var tableBody = '';
+        for (var k = 0; k < clueList.length; k++) {
+
+            tableBody += '<tr>';
+            tableBody += '<td>' +  clueList[k]['clue'] + '</td>';
+            tableBody += '<td>' +  clueList[k]['answer'] + '</td>';
+            tableBody += '</tr>';
+        }
+
+        // Put the table body we just built inside the table that's already on the page.
+        var resultsTableElement = document.getElementById('clue_table');
+        if (resultsTableElement) {
+            resultsTableElement.innerHTML = tableBody;
+        }
+    })
+
+    // Log the error if anything went wrong during the fetch.
+    .catch(function(error) {
+        console.log(error);
+    });
+
+}
+
+
+/*
+function onCluesButtonCicked() {
+    
+    var url = getAPIBaseURL() + '/clues/';
+
+    // Send the request to the Crosswords API /authors/ endpoint
+    fetch(url, {method: 'get'})
+
+    // When the results come back, transform them from a JSON string into
+    // a Javascript object (in this case, a list of author dictionaries).
+    .then((response) => response.json())
+
+    // Once you have your list of author dictionaries, use it to build
+    // an HTML table displaying the author names and lifespan.
+    .then(function(clueList) {
+        // Build the table body.
+        var tableBody = '';
+        for (var k = 0; k < clueList.length; k++) {
+
+            tableBody += '<tr>';
+            tableBody += '<td>' +  clueList[k]['clue'] + '</td>';
+            tableBody += '<td>' +  clueList[k]['answer'] + '</td>';
+            tableBody += '</tr>';
+        }
+
+        // Put the table body we just built inside the table that's already on the page.
+        var resultsTableElement = document.getElementById('clue_table');
+        if (resultsTableElement) {
+            resultsTableElement.innerHTML = tableBody;
+        }
+    })
+
+    // Log the error if anything went wrong during the fetch.
+    .catch(function(error) {
+        console.log(error);
+    });
+
+}
+
+
+
 
 function onCluesButtonCicked() {
     
@@ -61,7 +201,7 @@ function onCluesButtonCicked() {
 }
 
 
-/*
+
 function getClue(clueID, clueName) {
 
     // Very similar pattern to onAuthorsButtonClicked, so I'm not
